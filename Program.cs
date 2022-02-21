3a67f3ab46964f9e3c3e3aa045921ef01357baaf
hamster <dokalov@outlook.com>
2022-02-22 01:29:36 +0300

    added c# environment, code and block scheme

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿void PrintStringArray(string[] stringArray)
+{
+    Console.Write("[ ");
+    for (int i=0; i<stringArray.Length; i++)
+    {
+        Console.Write($"\"{stringArray[i]}\"");
+        if (i != stringArray.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+
+int searchStringsWithSizeLessOrEqual3(string[] stringArray)
+{
+    int numberOfStrings = 0;
+    for (int i=0; i<stringArray.Length; i++)
+    {
+        if (stringArray[i].Length <= 3)
+        {
+            numberOfStrings++;
+        }
+    }
+    return numberOfStrings;
+}
+
+string[] createArrayWithSizeLessOrEqual3(string[] stringArray, int sizeNewArray)
+{
+    string[] newStringArray = new string[sizeNewArray];
+    int j=0;
+    for (int i=0; i<stringArray.Length; i++)
+    {
+        if (stringArray[i].Length <= 3)
+        {
+            newStringArray[j] = stringArray[i];
+            j++;
+        }
+    }
+    return newStringArray;
+}
+
+string[] stringArray = { "C#", "Python", "Java", "C++", "SQL" };
+PrintStringArray(stringArray);
+int sortedNewArray = searchStringsWithSizeLessOrEqual3(stringArray);
+string[] newStringArray = createArrayWithSizeLessOrEqual3(stringArray, sortedNewArray);
+Console.Write(" => ");
+PrintStringArray(newStringArray);
+Console.WriteLine();
